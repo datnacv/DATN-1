d8b380d99f593b85ba9cd194187408cd69391333
deszyxh <syleminhhieu@gmail.com>
2025-07-02 17:40:54 +0700

    gio hang chua them duoc gio hang

---
+++ src/main/java/com/example/AsmGD1/entity/ChiTietGioHang.java
@@ -0,0 +1,47 @@
+package com.example.AsmGD1.entity;
+
+import com.example.AsmGD1.entity.ChiTietSanPham;
+import com.example.AsmGD1.entity.GioHang;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "gio_hang_chi_tiet")
+@Data
+public class ChiTietGioHang {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "UNIQUEIDENTIFIER")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_giohang", nullable = false)
+    @JsonIgnore
+    private GioHang gioHang;
+
+    @ManyToOne
+    @JoinColumn(name = "id_chi_tiet_san_pham", nullable = false)
+    private ChiTietSanPham chiTietSanPham;
+
+    @Column(name = "so_luong", nullable = false)
+    private Integer soLuong;
+
+    @Column(name = "gia", nullable = false, precision = 10, scale = 2)
+    private BigDecimal gia;
+
+    @Column(name = "tien_giam", precision = 10, scale = 2)
+    private BigDecimal tienGiam;
+
+    @Column(name = "ghi_chu", columnDefinition = "NVARCHAR(MAX)")
+    private String ghiChu;
+
+    @Column(name = "thoi_gian_them", nullable = false)
+    private LocalDateTime thoiGianThem;
+
+    @Column(name = "trang_thai", nullable = false)
+    private Boolean trangThai;
+}
---
+++ src/main/java/com/example/AsmGD1/entity/GioHang.java
@@ -0,0 +1,44 @@
+package com.example.AsmGD1.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "giohang")
+@Data
+public class GioHang {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "UNIQUEIDENTIFIER")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_nguoi_dung", nullable = false)
+    private NguoiDung nguoiDung;
+
+    @Column(name = "ma_gio_hang", nullable = false, length = 20)
+    private String maGioHang;
+
+    @Column(name = "tong_tien", precision = 15, scale = 2)
+    private BigDecimal tongTien;
+
+    @Column(name = "thoi_gian_tao", nullable = false)
+    private LocalDateTime thoiGianTao;
+
+    @Column(name = "trang_thai", nullable = false)
+    private Boolean trangThai;
+
+    @OneToMany(mappedBy = "gioHang", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<ChiTietGioHang> gioHangChiTiets;
+
+    public void addGioHangChiTiet(ChiTietGioHang chiTiet) {
+        gioHangChiTiets.add(chiTiet);
+        chiTiet.setGioHang(this);
+    }
+}
