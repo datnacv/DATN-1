097922cea608b1e17904c4523d15986a47f28e48
concarong <pdm25122006@gmail.com>
2025-07-23 19:28:57 +0700

    Gộp Mạnh Long Nam

--- src/main/java/com/example/AsmGD1/config/SecurityConfig.java
+++ src/main/java/com/example/AsmGD1/config/SecurityConfig.java
@@ -269,7 +269,7 @@ public class SecurityConfig implements ApplicationContextAware {
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/acvstore/login", "/acvstore/verify-face", "/customers/login", "/customers/oauth2/register", "/api/cart/check-auth", "/api/cart/get-user","/css/**", "/js/**", "/images/**", "/vi/**").permitAll()
+                        .requestMatchers("/","/acvstore/login", "/acvstore/verify-face", "/customers/login", "/customers/oauth2/register", "/api/cart/check-auth", "/api/cart/get-user","/css/**", "/js/**", "/image/**", "/vi/**").permitAll()
                         .requestMatchers("/cart", "/api/cart/**").authenticated()
                         .anyRequest().authenticated()
                 )
--- src/main/java/com/example/AsmGD1/repository/GiamGia/PhieuGiamGiaCuaNguoiDungRepository.java
+++ src/main/java/com/example/AsmGD1/repository/GiamGia/PhieuGiamGiaCuaNguoiDungRepository.java
@@ -1,14 +1,14 @@
-    package com.example.AsmGD1.repository.GiamGia;
+package com.example.AsmGD1.repository.GiamGia;
 
-    import com.example.AsmGD1.entity.PhieuGiamGiaCuaNguoiDung;
-    import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.AsmGD1.entity.PhieuGiamGiaCuaNguoiDung;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-    import java.util.List;
-    import java.util.Optional;
-    import java.util.UUID;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-    public interface PhieuGiamGiaCuaNguoiDungRepository extends JpaRepository<PhieuGiamGiaCuaNguoiDung, UUID> {
-        List<PhieuGiamGiaCuaNguoiDung> findByPhieuGiamGia_Id(UUID phieuId);
-        List<PhieuGiamGiaCuaNguoiDung> findByNguoiDungId(UUID nguoiDungId);
-        Optional<PhieuGiamGiaCuaNguoiDung> findByPhieuGiamGia_IdAndNguoiDung_Id(UUID phieuGiamGiaId, UUID nguoiDungId);
-    }
+public interface PhieuGiamGiaCuaNguoiDungRepository extends JpaRepository<PhieuGiamGiaCuaNguoiDung, UUID> {
+    List<PhieuGiamGiaCuaNguoiDung> findByPhieuGiamGia_Id(UUID phieuId);
+    List<PhieuGiamGiaCuaNguoiDung> findByNguoiDungId(UUID nguoiDungId);
+    Optional<PhieuGiamGiaCuaNguoiDung> findByPhieuGiamGia_IdAndNguoiDung_Id(UUID phieuGiamGiaId, UUID nguoiDungId);
+}
