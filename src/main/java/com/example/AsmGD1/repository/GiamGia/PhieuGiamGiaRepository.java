53bb6724f58f1aa48132697793eee0d50994448d
NamHoang <namhaihoang3103@gmail.com>
2025-07-25 02:43:55 +0700

    done đăng ký

--- src/main/java/com/example/AsmGD1/repository/GiamGia/PhieuGiamGiaRepository.java
+++ src/main/java/com/example/AsmGD1/repository/GiamGia/PhieuGiamGiaRepository.java
@@ -4,8 +4,11 @@ import com.example.AsmGD1.entity.PhieuGiamGia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface PhieuGiamGiaRepository extends JpaRepository<PhieuGiamGia, UUID>, JpaSpecificationExecutor<PhieuGiamGia> {
     boolean existsByMaIgnoreCase(String ma);
+    Optional<PhieuGiamGia> findByMaIgnoreCase(String ma);
+
 }
